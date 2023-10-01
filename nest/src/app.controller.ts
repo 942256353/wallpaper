af0dec0bdd0f39da358fdfc3b950cbe76364cb1c
942256453 <942256453@qq.com>
2023-10-01 19:41:57 +0800

    修改后端图片获取来源

--- nest/src/app.controller.ts
+++ nest/src/app.controller.ts
@@ -1,31 +1,13 @@
-import { Controller, Get, Res } from '@nestjs/common';
+import { Controller, Get, Res, } from '@nestjs/common';
 import { AppService } from './app.service';
-import { readFileSync, readdirSync } from 'fs';
-import { resolve } from 'path';
-import _ from 'lodash';
-import { Response } from 'express';
-import { readFile, readdir } from 'fs/promises';
+
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello() {
-    const files = await readdir(resolve(__dirname, '..', 'wallpaper'));
-    return (
-      'http://43.138.152.177:3001/wallpaper/' + files[_.random(files.length - 1)]
-    );
-    //返回 buffer
-    //   const file = await readFile(
-    //     resolve(__dirname, '../wallpaper', files[_.random(files.length - 1)]),
-    //   );
-    //  return new Promise(resolve =>{
-    //   setTimeout(()=>{
-    //     res.type('image/png');
-    //     // res.set('Content-Type', 'image/*')
-    //     resolve(res.send(file))
-    //   },2000)
-    //  })
+  async getImage() {
+    return this.appService.getImage();
   }
 }
--- nest/src/app.service.ts
+++ nest/src/app.service.ts
@@ -1,8 +1,44 @@
 import { Injectable } from '@nestjs/common';
+import { readFileSync, readdirSync } from 'fs';
+import { resolve } from 'path';
+import _ from 'lodash';
+import { Response } from 'express';
+import { readFile, readdir } from 'fs/promises';
+import fecth from 'node-fetch';
+
+
+const imgUrl_1 = 'http://cdn.apc.360.cn/index.php?c=WallPaper&a=getAllCategoriesV2&from=360chrome';
+const imgUrl_2 = (id, start = 1) => (`http://wallpaper.apc.360.cn/index.php?c=WallPaper&a=getAppsByCategory&cid=${id}&start=${start}&count=1&from=360chrome`)
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getImage() {
+   try {
+      const res = await fecth(imgUrl_1).then(r => r.json())
+      const resId = res.data.map(v => v.id)
+      const id = resId[_.random(resId.length - 1)]
+      const res_1 = await fecth(imgUrl_2(id)).then(r => r.json()) 
+      const start = _.random(res_1.total)==0?1:_.random(res_1.total)
+      const data = await fecth(imgUrl_2(id,start)).then(r => r.json())
+      return data.data[0].url
+    } catch (error) {
+      const files = await readdir(resolve(__dirname, '..', 'wallpaper'));
+      return (
+        'http://43.138.152.177:3001/wallpaper/' + files[_.random(files.length - 1)]
+      );
+    }
+
+    //返回 buffer
+    //   const file = await readFile(
+    //     resolve(__dirname, '../wallpaper', files[_.random(files.length - 1)]),
+    //   );
+    //  return new Promise(resolve =>{
+    //   setTimeout(()=>{
+    //     res.type('image/png');
+    //     // res.set('Content-Type', 'image/*')
+    //     resolve(res.send(file))
+    //   },2000)
+    //  })
   }
 }
